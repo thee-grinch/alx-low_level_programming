44897cc37385f862dcd4e9579f545c1f16a746b4
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-21 11:59:46 -0800

    trial 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,22 +3,14 @@
  * main - prints all alphabets in lowercase
  * Return: always 0 (success)
  */
-int main(void)
+void print_alphabet(void)
 {
-	/*
-	 * main - check the code
-	 * Return: always 0
-	 */
-	void print_alphabet(void)
-	{
-		char c;
+	char c;
 
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
 	}
-	print_alphabet();
-	return (0);
-}
+	_putchar('\n');
+	}
+
