80a125c82002bc873b20a2e902b98b2b32f0a7c3
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-21 11:00:06 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/* main - prints _putchar
+ * Return: always 0 success
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return 0;
+}
