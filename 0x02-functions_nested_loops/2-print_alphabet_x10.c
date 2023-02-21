58b12b9721aceb2ee1db7ce15b92adf6e29d213b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-21 13:14:52 -0800

    trial 6

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *  in lowercase, followed by a new line
+ *  Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int times = 0;
+
+	char c;
+
+	while (times < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		times++;
+	}
+}
