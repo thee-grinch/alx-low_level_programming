822ca99bc51a83e2d5cf7b65696f6103d3ed0564
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-21 18:03:26 -0800

    trial 19

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - a function to add 2 integers
+ * @x: int to be added
+ * @y: int to be added
+ * Return: sum
+ */
+int add (int x, int y)
+{
+	int z;
+	
+	z = x + y;
+	
+	return (z);
+}
