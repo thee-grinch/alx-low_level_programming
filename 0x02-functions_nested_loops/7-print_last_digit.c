f7fdbbc8d9cc778cffb77fa9fdb42f1b75f964fb
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-21 16:00:45 -0800

    trial 14

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * print_last_digit - function that prints the last digit of a number
+ * @c: is the character to be computed
+ * Return:is the output
+ *
+ */
+int print_last_digit(int)
+{
+	return (n % 10);
+}
