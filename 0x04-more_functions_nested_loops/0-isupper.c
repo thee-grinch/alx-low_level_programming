84b2d4ec7161b3ea4087fb31212ce767b180232a
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-23 07:49:39 -0800

    trial 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - checks whether the character is uppercase
+* @c: the character to be checked
+* Return: always 1
+*/
+int _isupper(int c)
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
