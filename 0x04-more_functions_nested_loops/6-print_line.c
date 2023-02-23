102d4c36cbfc9c4f0a39e24af83d4254592e230a
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-23 15:24:03 -0800

    Trial 10

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_line - Entry point
+* @n: number of times the character _ should be printed
+*/
+void print_line(int n)
+{
+	int i;
+
+	while (i <= n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	i++;
+	}
+}
