6e8e6c07b8be03e650b5041b2bd32f87352606fd
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-23 14:14:22 -0800

    Triall 6

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_numbers - function to print numbers
+*Return: void
+*/
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
