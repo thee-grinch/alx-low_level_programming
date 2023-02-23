54129cf90d51d61e84575fb04240ffa731660293
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-23 08:26:53 -0800

    Trial 3

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - entry point
+*@c: integer to be checked
+*Return: always 1
+*/
+int _isdigit(int c)
+{
+	if (c >= '0'; &&c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
