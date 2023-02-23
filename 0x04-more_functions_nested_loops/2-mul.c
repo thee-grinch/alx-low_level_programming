51957b962c3293bb9ddf5359b8502583b0f8f5df
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-23 09:07:39 -0800

    Trial 5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - function to multiply 2 numbers
+*@a: number to be multiplied
+*@b: number to be multiplied
+*Return: multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
