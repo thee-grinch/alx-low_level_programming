8be573d564c2cb9e4fd647655cbbd6a2064ef575
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-27 13:22:04 -0800

    Trial 4

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
-#include "main.h";
+#include "main.h"
 /**
 *swap_int - check the code
-*@a: to be swapped
-@b: to be swapped
+*@a:to be swapped
+*@b:to be swapped
 *
 */
 void swap_int(int *a, int *b)
 {
 	int c = *a;
-
+	
 	*a = *b;
 	*b = c;
 }
