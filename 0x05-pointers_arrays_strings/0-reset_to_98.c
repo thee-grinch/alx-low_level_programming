dc39eddbb0c9fc567c735358764315c29671c07b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-27 12:18:32 -0800

    Trial 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*reset_to_98 - check the code
+*@*n: pointer to be converted
+*
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder is about pointer arrays and string sample questions
