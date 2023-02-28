3de2d97cc4cadcca47849adb93b9448bb2bc32f3
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-02-27 16:11:26 -0800

    tiall 23

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,12 @@
 */
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 1;
 
 	_putchar(str[0]);
-	while (str[i + 1] != '\0')
+	while (str[i] != '\0')
 	{
-		if (str[i] % 2 == 0)
+		if (i % 2 == 0)
 			_putchar(str[i]);
 		i++;
 	}
