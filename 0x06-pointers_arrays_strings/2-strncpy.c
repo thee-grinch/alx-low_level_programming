d95a1e51fec21a10223255dede6a18f9afc304df
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-01 13:45:38 -0800

    Trial 5

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_strncpy - check the code
+*@dest: string 1
+*@src: string 2
+*@n: number of characters
+*Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
