feca2a437ff50b400cca88faa58e7d4f9508eb19
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-02 13:36:23 -0800

    Trial 14

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+/**
+*leet - check the code
+*@s: string one
+* Return: pointer to the encoded string
+*/
+char *leet(char *s)
+{
+	char *r = s;
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+				*s = n[i] + '0';
+		}
+		s++;
+	}
+	return (r);
+}
