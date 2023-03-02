424e405ace04a08f7173efd8edb378a67fa4fc8e
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-02 12:58:27 -0800

    Trial 12

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*cap_string - check the code
+*@s:string
+*Return: check the code
+*/
+char *cap_string(char *s)
+{
+	int i, j;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{'};
+
+	sep[12] = '}';
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; sep[j]; j++)
+		{
+			if (sep[j] == s[i])
+			{
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
