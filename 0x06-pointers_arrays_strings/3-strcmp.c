a756fae30961a522c563d17a14c6ac9005148e4b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-02 05:36:34 -0800

    Trial 9

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,21 +8,25 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int ans;
 
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
 		{
-			return (15);
+			ans = 15;
+			break;
 		}
 		else if (s2[i] > s1[i])
 		{
-			return (-15);
+			ans = -15;
+			break;
 		}
 		else
 		{
 			i++;
 		}
-		return (0);
+		ans = 0;
 	}
+	return (ans);
 }
