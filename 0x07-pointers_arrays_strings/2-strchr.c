c1803527701cbcc01ba9eb45d8d1840816de9d39
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-06 14:26:06 -0800

    question 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_strchr - check the code
+*@s: main string
+*@c: substring
+*/
+char *_strchr(char *s, char c)
+{
+	while (s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	return (0);
+}
