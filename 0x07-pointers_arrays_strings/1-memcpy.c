17bfc4bc29b609d4c47f80d37e703cd063fe1960
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-06 03:55:53 -0800

    Trial 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,16 +4,13 @@
 *@dest: destination
 *@src: source
 *@n: number of characters
-*
+*Return: dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *d = dest;
-	char *s = src;
+	int i;
 
-	while (n--)
-	{
-		*s = *d;
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
