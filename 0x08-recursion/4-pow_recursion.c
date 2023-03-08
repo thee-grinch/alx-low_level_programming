d0017576715a6c012531ee28477b4a2d3f8b00c3
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-08 14:21:22 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,11 +3,14 @@
 *_pow_recursion - check the code
 *@x: the base
 *@y: the power
+*Return: power
 */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
+	else if (y == 0)
+		return (1);
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
