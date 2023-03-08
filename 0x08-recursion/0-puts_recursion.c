ec7703a3467cd81ae4f94fc23b080427cf8a3a31
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-08 11:34:44 -0800

    Trial 2

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_puts_recursion - check the code
+*@s: string to be checked
+*/
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
