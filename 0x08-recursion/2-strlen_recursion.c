a19e6e1041eb0939cebb44c9c94943e278b7d3ee
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-08 13:10:33 -0800

    ty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,11 @@
 */
 int _strlen_recursion(char *s)
 {
-	while (*s)
-	{
-	if (*s == '0')
+	if (*s == '\0')
 	{
 		return (0);
 	}
 	else
-		return (1 + _strlen_recursion(s + 1) + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
