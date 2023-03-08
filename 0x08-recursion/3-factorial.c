141f26eb53b4efc08cfcc505b14d43b5c2d0b309
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-03-08 13:49:37 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*factorial - check the code
+*@n: to be checked
+*Return: the factorial
+*/
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1);
+}
