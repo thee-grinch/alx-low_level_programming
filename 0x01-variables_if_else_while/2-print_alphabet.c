d34146b3297d2cca566c34cc0c81643eac3e0c65
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-19 12:15:39 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: always zero
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+		putchar(c);
+	putchar(10);
+	return (0);
+}
