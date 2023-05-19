884ad514d2fe29dbb19ba103cb48c7b211bae1ae
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-19 13:07:59 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: always zero
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	putchar(10);
+	return (0);
+}
