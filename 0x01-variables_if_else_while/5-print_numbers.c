16d7af94255b3ffa92db3b77c1bce268b9d93ac1
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-19 13:01:12 +0300

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -  check the code
+ * Return: always zero
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar(10);
+	return (0);
+}
