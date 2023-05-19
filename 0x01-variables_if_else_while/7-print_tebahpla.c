c3760e16b141529b3e25b893645b75a2d6ce5ba5
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-19 13:27:40 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: always zero
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 122; c >= 97; c--)
+		putchar(c);
+	putchar(10);
+	return (0);
+}
