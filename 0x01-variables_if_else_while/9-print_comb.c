4ebfa2da72d736158fec1141e9dd3adf96cfeda2
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-19 15:05:20 +0300

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,9 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c == '9')
+		putchar(c);
+		if (c != '9')
 		{
-			putchar(c);
-		}
-		else
-		{
-			putchar(c);
 			putchar(',');
 			putchar(' ');
 		}
