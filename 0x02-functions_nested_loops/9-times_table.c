22923700c29292ce9f00e476954a7eddcbd91090
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-23 17:49:33 +0300

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,17 +13,19 @@ void times_table(void)
 			k = i * j;
 			if (k <= 9)
 				_putchar('0' + k);
+				_putchar(',');
+				_putchar(' ');
 			else
 			{
 				_putchar('0' + (k / 10));
 				_putchar('0' + (k % 10));
+				_putchar(',');
 			}
 			if (j == 9)
 			{
 				_putchar('\n');
 				continue;
 			}
-			_putchar(',');
 			_putchar(' ');
 		}
 		j = 0;
