acb3b83902b7c1b2c80e2cc26c123acdad774387
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-23 09:24:40 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _putchar1 - prints the function name
+ * Return: always zero
+ * @c: printed character
+ */
+int _putchar1(int c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
