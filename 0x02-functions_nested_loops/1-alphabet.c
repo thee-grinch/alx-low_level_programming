7cdf0743feec07a96f5b4af9c994df9b0994b40f
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-23 09:36:42 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: always zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+	return (0);
+}
