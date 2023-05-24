3a5861ae9933d8de3b2bb03187f7a53f171dc7fa
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-24 21:25:38 +0300

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,19 +6,17 @@
 int main(void)
 {
 	int sum, num;
+
 	sum = 0;
 
 	for (num = 0; num < 1024; num++)
 	{
 		if (num % 3 == 0 || num % 5 == 0)
 		{
-			printf("%d", num);
 			sum += num;
-			if (num == 1023)
-				putchar(10);
-			else
-				printf(", ");
 		}
 	}
+	printf("%d", sum);
+	putchar(10);
 	return (sum);
 }
