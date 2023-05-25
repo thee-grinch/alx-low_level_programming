6c92d40c88bc1494c02712ac0b64e1229540a511
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-25 14:32:49 +0300

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - check the code
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+		}
+		_putchar(10);
+	}
+}
