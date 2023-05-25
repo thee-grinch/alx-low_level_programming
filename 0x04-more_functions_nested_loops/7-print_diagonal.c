f6a4b9d751d36c7cada69a2a1d79a693155b8afd
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-25 19:15:09 +0300

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,11 +7,16 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j <= i; j++)
-			_putchar(' ');
-		_putchar('\\');
+	if (n <= 0)
 		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
