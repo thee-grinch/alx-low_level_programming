6f24305a85c263d624e286d746fff22097d5f13c
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-25 14:17:58 +0300

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		_putchar(i);
+	}
+	_putchar(10);
+}
