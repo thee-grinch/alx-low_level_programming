8dafa1a6f63e1948ff6bd594fab56d4cf4eda1a7
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-26 10:41:23 +0300

    Print ints

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_triangle  - prints a triangle
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, l, k;
+
+	k = size;
+	for (i = 1; i < k; i++)
+	{
+		_putchar(' ');
+		for (l 
+
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_number -prints an integer
+ * @n: the number to be printed
+ */
+void print_number(int n)
+{
+	int rev, i, k, j;
+
+	rev = 0;
+	if (n < 0)
+		j = -n;
+	if (n > 0)
+		j = n;
+	if (n == 0)
+		_putchar('0');
+	while (j > 0)
+	{
+		i = j % 10;
+		rev += i;
+		if (j >= 10)
+			rev *= 10;
+		j /= 10;
+	}
+	if (n < 0)
+		_putchar('-');
+	while (rev > 0)
+	{
+		_putchar('0' + rev % 10);
+		rev /= 10;
+	}
+	_putchar('\n');
+}
+
