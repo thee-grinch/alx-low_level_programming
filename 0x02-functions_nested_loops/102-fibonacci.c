19756c21272c8c0ebaaa9e47b8c72e199410b591
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-28 17:14:56 +0300

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,15 +4,15 @@
  * main - prints fibbonacci numbers
  * Return: always 9
  */
-int main (void)
+int main(void)
 {
 	int i;
-       long int	n1, n2, n3;
+	long int n1, n2, n3;
 
 	n1 = 1;
 	n2 = 2;
 	printf("%ld, %ld, ", n1, n2);
-	for (i = 0; i <= 48; i++)
+	for (i = 0; i < 48; i++)
 	{
 		n3 = n1 + n2;
 		printf("%ld", n3);
