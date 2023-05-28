31864048d70505047fe6ecf63a1994ccb9c4f4fa
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-28 21:01:19 +0300

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,11 +11,12 @@ int main(void)
 
 	n1 = 1;
 	n2 = 2;
-	sum = 3;
-	for (i = 0; i < 4000000; i++)
+	sum = 2;
+	while (n3 <= 4000000)
 	{
 		n3 = n1 + n2;
-		sum += n3;
+		if (n3 % 2 == 0)
+			sum += n3;
 		n1 = n2;
 		n2 = n3;
 	}
