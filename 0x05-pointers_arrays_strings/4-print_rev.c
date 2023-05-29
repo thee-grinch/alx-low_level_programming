8b1f17fb4bb5753fb99fa57dcb766f066a46205b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-29 22:27:56 +0300

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_rev - check the code
+ * @s: the string to be reversed
+ *
+ */
+int _strlen(char *s)
+{
+        int i, len;
+
+        len = 0;
+        if (s)
+        {
+                for (i = 0; s[i] != '\0'; i++)
+                {
+                        len++;
+                }
+        }
+        return (len);
+}
+void print_rev(char *s)
+{
+	int i, len;
+
+	if (s)
+	{
+		len = _strlen(s);
+		for (i = len - 1; i >= 0; i--)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+	}
+}
