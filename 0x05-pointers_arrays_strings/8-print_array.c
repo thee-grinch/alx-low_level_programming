f5401d261029c9a074a8448c09c729bd609a7fd1
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-30 21:07:01 +0300

    9-strcpy.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,15 +2,20 @@
 /**
  * print_array - prints the first n terms
  * @n: the number of characters to be printed
+ * @a: the array
  */
 void print_array(int *a, int n)
 {
 	int i;
 
+	if (n < 0)
+	{
+		printf(" \n");
+	}
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if ( (n - i) == 1)
+		if ((n - i) == 1)
 			printf("\n");
 		else
 			printf(", ");
