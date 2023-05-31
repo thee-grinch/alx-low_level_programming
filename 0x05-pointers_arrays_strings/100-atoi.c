eabb9c6a797d14569b1982ac57731c6ff94f584d
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-05-31 11:32:59 +0300

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _atoi - works the same as atoi
+ * @s: the atoi integer
+ * Return: the int
+ */
+int _atoi(char *s)
+{
+	int sign, rev, i;
+
+	rev = 0;
+	sign = 1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			rev = rev * 10 + (s[i] - '0');
+			if (s[i + 1] >= '0' && s[i + 1] <= '9')
+				continue;
+			else
+				break;
+		}
+	}
+	rev = rev * sign;
+	return (rev);
+}
