ff0bc8011623b810c81bd3694da68a1e71f9c826
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-01 17:47:53 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,16 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	if (s1 > s2)
-		i = *s1 - 0;
-	else if (s2 > s1)
-		i = 0 - *s1;
+	for (i = 0; s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'); i++)
+	{
+		if (s1[i] == s2[i])
+			continue;
+		if (s1[i] == '\0' || s2[i] == '\0')
+			break;
+	}
+	if (s1[i] == s2[i])
+		return (0);
 	else
-		i = 0;
-	return (i);
+		return (s1[i] - s2[i]);
+	return (0);
 }
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code for
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
     return (0);
 }
