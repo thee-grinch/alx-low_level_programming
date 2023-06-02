f5a4bb39f792d04b901b8500ec0b2514e668e3e0
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-02 23:17:44 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 101-print_number.c test.c -o test

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,23 +9,26 @@ void print_number(int n)
 	unsigned int reverse = 0;
 	char sign = '+';
 
-	if (n == 0)
-		_putchar('0');
 	if (n < 0)
 	{
 		sign = '-';
 		n = -n;
 	}
-	while (n > 0)
-	{
-		reverse = reverse * 10 + (n % 10);
-		n /= 10;
-	}
 	if (sign == '-')
 		_putchar('-');
-	while (reverse > 0)
+	if (n >= 0 && n <= 9)
+		_putchar('0' + n);
+	if (n >= 10)
 	{
-		_putchar('0' + (reverse % 10));
-		reverse /= 10;
+		while (n > 0)
+		{
+			reverse = reverse * 10 + (n % 10);
+			n /= 10;
+		}
+		while (reverse > 0)
+		{
+			_putchar('0' + (reverse % 10));
+			reverse /= 10;
+		}
 	}
 }
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,12 +7,19 @@
  */
 int main(void)
 {
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-99);
+    _putchar('\n');
+    print_number(-1);
+    _putchar('\n');
+    print_number(-4096);
+    _putchar('\n');
     return (0);
 }
-
