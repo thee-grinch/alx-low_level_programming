b69db69e7e340fab835780f65c9a75839edfdf22
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-02 23:33:53 +0300

    I forced my self to try using recursion because, wueh

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,28 +7,15 @@
 void print_number(int n)
 {
 	unsigned int reverse = 0;
-	char sign = '+';
 
 	if (n < 0)
 	{
-		sign = '-';
-		n = -n;
-	}
-	if (sign == '-')
 		_putchar('-');
-	if (n >= 0 && n <= 9)
-		_putchar('0' + n);
-	if (n >= 10)
-	{
-		while (n > 0)
-		{
-			reverse = reverse * 10 + (n % 10);
-			n /= 10;
-		}
-		while (reverse > 0)
-		{
-			_putchar('0' + (reverse % 10));
-			reverse /= 10;
-		}
+		n *= -1;
 	}
+	reverse = n;
+
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
