129d3e3c82f50922487b8f67f9eb044744060506
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-07 13:10:33 +0300

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * squareroot - finds the squareroot of a number
+ * @n: the the number
+ * Return: the squareroot
+ * @g: the number of times
+ */
+int squareroot(int n, int g)
+{
+	if (g * g == n)
+		return (g);
+	if (g * g < n)
+		return (-1);
+	return (squareroot(n, g - 1));
+}
+/**
+ * _sqrt_recursion - checks the squareroot
+ * @n: the number
+ * Return: the squareroot
+ */
+int _sqrt_recursion(int n)
+{
+	return (squareroot(n, n));
+}
