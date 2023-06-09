f6631deb120f489e2c31971c25d120257cc29a5f
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-09 20:48:06 +0300

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-mul.c -o mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,10 +9,8 @@
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
-	{
-		puts("Error\n");
-		return (0);
-	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("Error\n");
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
