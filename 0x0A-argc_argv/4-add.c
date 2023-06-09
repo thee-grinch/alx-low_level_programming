18c1effb9385eb66611c531b385e9728c745b299
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-09 21:02:21 +0300

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds up
+ * @argc: count
+ * @argv: yhe argument count
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum, a;
+
+	if (argc == 1)
+	{
+		puts("0");
+		return (0);
+	}
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		a = atoi(argv[i]);
+		if (a == 0)
+		{
+			puts("Error");
+			return (1);
+		}
+		else
+			sum += a;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
