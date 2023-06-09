7b66da04ebbbc812922a1c77fd480e2df9b57e35
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-09 19:50:57 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument vector;
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
