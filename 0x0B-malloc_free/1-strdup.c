2c0963c0bf57aad99c87992b04057c8b1efbf70e
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-12 11:13:09 +0300

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * length - calculatest the length of a string
+ * @s: the string to be checked the length
+ * Return: the length
+ */
+int length(char *s)
+{
+	int i, len;
+
+	len = 0;
+	if (s)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+			len++;
+	}
+	return (len);
+}
+/**
+ * _strdup - duplicates a string
+ * @str: the string to be duplicated
+ * Return: the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *duplicate;
+
+	len = length(str);
+	duplicate = malloc(len * sizeof(char) + 1);
+	if (duplicate != NULL)
+	{
+		for (i = 0; i < len; i++)
+		{
+			duplicate[i] = str[i];
+		}
+		duplicate[i] = '\0';
+		return (duplicate);
+	}
+	return (NULL);
+}
