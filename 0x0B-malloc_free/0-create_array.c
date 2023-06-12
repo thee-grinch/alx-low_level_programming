d75b59e811bbd2fc6dff522a6628bff20d5946c6
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-12 10:40:37 +0300

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,16 @@ char *create_array(unsigned int size, char c)
 	char *arrptr;
 	unsigned int i;
 
-	arrptr = malloc(size * sizeof(char));
-	if (arrptr == NULL)
+	if (size == 0)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	arrptr = malloc(size * sizeof(char));
+	if (arrptr != NULL)
 	{
-		arrptr[i] = c;
+		for (i = 0; i < size; i++)
+		{
+			arrptr[i] = c;
+		}
+		return (arrptr);
 	}
-	return (arrptr);
+	return (NULL);
 }
