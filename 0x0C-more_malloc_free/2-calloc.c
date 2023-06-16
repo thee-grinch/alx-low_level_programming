a36588075152003994b7da8849643a733db7416a
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-16 09:06:47 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc _ works the same as caloc
+ * _calloc - works the same as caloc
  * @nmemb: number of members
  * @size: the size of members
  * Return: always zero
@@ -9,18 +9,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	void *mem;
-	unsigned char *ptr;
+	char *mem;
+	void *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	mem = malloc(nmemb * size);
 	if (mem == NULL)
 		return (NULL);
-	ptr = (unsigned char *)mem;
 	for (i = 0; i < nmemb * size; i += size)
 	{
-		ptr[i] = 0;
+		mem[i] = 0;
 	}
-	return (mem);
+	ptr = (void *)mem;
+	return (ptr);
 }
