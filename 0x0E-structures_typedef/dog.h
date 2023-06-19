cad0b0f562a6bab97bb04294d8f67e8fc1fdc803
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-19 19:19:10 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the dog details
+ * @name: the dogs name
+ * @age: the dogs age
+ * @owner: the dogs owner
+ *
+ * Description: this one now you can check the code
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
