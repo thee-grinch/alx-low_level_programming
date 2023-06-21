83ca19c09dcd5e4d1d6a4181de1414d9ad052226
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-21 18:22:34 +0300

    to Hell

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - returns the index
+ * @array: the array of numbers
+ * @size: the array size
+ * @cmp: the function pointer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
