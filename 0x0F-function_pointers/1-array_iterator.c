0ea705f4d707ed2c8c9ffe0f656b5db49f6c3b21
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-21 12:47:48 +0300

    number 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * @array: the number of parameters
+ * @size: the size of the array
+ * @action: the action pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
