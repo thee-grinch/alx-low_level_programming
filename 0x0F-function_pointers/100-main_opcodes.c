8a3c45adb14f9c20adfb72d2053fef0a81c48861
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-06-22 05:56:06 +0300

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print - prints the opcodes
+ * @s: the string
+ */
+void print(int num)
+{
+	unsigned char *ptr = (unsigned char *)&print;
+
+	for (int i = 0; i < num; i++)
+	{
+		printf("%02x ", *(ptr + i));
+       	}
+
+	printf("\n");
+}
+/**
+ * main - prints opcodes
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: always zero
+ */
+int main(int argc, char *argv[]) {
+	int num;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print(num);
+	return 0;
+}
