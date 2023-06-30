5ae1777d796f7afc81b14bfc6d3e6084958385e7
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-07-01 02:48:39 +0300

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a linked list
+ * @head: the lists head
+ */
+void free_list(list_t *head)
+{
+	list_t *Next;
+
+	while (head)
+	{
+		next = head->next;
+		if (head->str != NULL)
+			free(head->str);
+		free(head);
+		head = Next;
+	}
+}
