3904ae0b924e23e72cafe71965c0ace26a92df8e
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-07-04 10:03:37 +0300

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * free_listint2 - frees a listint structure
  * @head: pointer to a pointer pointing to the head node
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *buffer = *head;
+	listint_t *buffer;
 
-	while (buffer)
+	while (*head)
 	{
-		buffer = buffer->next;
+		buffer = (*head)->next;
 		free(*head);
 		*head = buffer;
 	}
