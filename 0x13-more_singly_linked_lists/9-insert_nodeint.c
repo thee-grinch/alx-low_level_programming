6fab9cbdb58d7a04f5b9a4ccbffd535409ba6c7a
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-07-05 10:17:37 +0300

    try fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	buffer = *head;
 	while (buffer)
 	{
+		if (idx == 0)
+		{
+			new_node->next = head;
+			head = new_node;
+			return (new_node);
+		}
 		if (i == idx - 1)
 		{
 			next = buffer->next;
