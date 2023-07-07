1d1a4d3ed0ed97a5c417631a387330bb00071fc2
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-07-07 13:05:13 +0300

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_bit -  gets a bit at a certain index
+ * @n: the number to be checked
+ * @index: theindex of the number to be cheched
+ * Return: the digit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+
+	if (index > 63)
+		return (-1);
+	i = (n >> index) & 1;
+
+	return (i);
+}
