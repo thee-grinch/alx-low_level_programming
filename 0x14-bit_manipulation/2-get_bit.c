707741a079eb5c075b45d84273ce5b04af483b10
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-07-07 15:26:53 +0300

    try another  one

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,11 +8,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int i;
+	unsigned int i;
 
 	if (index > 63)
 		return (-1);
 	i = (n >> index) & 1;
 
-	return (i);
+	return (i & 1);
 }
