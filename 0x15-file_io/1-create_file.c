97f2d50b33e2c2be0a7a98d99ad65b5d5dfc3d16
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-07-12 11:10:37 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC | O_EXCL, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (op == -1)
 		return (-1);
 	if (!text_content)
