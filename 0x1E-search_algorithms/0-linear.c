4be612dacdaf481306b1ca1d1bc5b8c3bfabaed4
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-01-17 09:54:22 +0300

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - implementation of a linear search algorithm
+ * @array: the array to be searched
+ * @size: the size of the array
+ * @value: the value to be searched
+ * Return: index or -1 on error
+ *
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i,  array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
